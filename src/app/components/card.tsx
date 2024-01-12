7626fa21e0aee4ba84fa21e1a3b1d217c8b2431d
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-01-12 17:13:35 -0300

    add animation in project card

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -9,7 +9,7 @@ export interface props {
 
 export default function Card(props: props) {
     return (
-        <a href={props.link} target="_blank" className="flex flex-col items-center rounded-lg border border-gray-200 bg-gray-800 shadow hover:bg-gray-500 md:max-w-xl md:flex-row dark:border-gray-700 dark:hover:text-white">
+        <a href={props.link} target="_blank" className="flex flex-col items-center rounded-lg border border-gray-200 bg-gray-800 shadow transition ease-in-out hover:-translate-y-1 hover:scale-105 md:max-w-xl md:flex-row dark:border-gray-700 dark:hover:text-white">
             <Image className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={props.image} alt="project image" />
             <div className="flex flex-col justify-between p-4 text-center leading-normal md:text-left">
                 <h5 className="mb-2 text-2xl font-bold italic tracking-tight text-gray-900 dark:text-lavender">{props.title}</h5>
