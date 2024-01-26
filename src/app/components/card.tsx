5ee75d7a47d5da51afd90a0ad4d06f0427d73621
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-01-26 17:26:06 -0300

    more responsive in bento grid

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -12,11 +12,13 @@ export default function Card(props: props) {
     return (
         <div
             className={`group row-span-1 cursor-pointer rounded-xl border-2 border-slate-400/10 bg-local text-white ${
-                props.i === 0 || props.i === 3  || props.i === 4 ? "col-span-1 md:col-span-2" : ""
+                props.i === 0 || props.i === 3 || props.i === 6 ? "col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-3" : props.i === 2 ? 'row-span-2' : ''
             }`}
         >
-            <span className="absolute z-50 hidden text-white  group-hover:flex group-hover:opacity-100">{props.i}</span>
-            <Image src={props.image} width={1} alt={props.title} height={1} className="h-full w-full rounded-xl object-cover transition-opacity duration-300 group-hover:brightness-50" />
+            <div className="absolute z-50">
+                <h2 className=" hidden p-4 text-white  group-hover:flex group-hover:opacity-100">{props.i}</h2>
+            </div>
+            <Image src={props.image} width={1} alt={props.title} height={1} className=" h-full w-full rounded-xl object-cover transition-opacity duration-300 group-hover:brightness-50" />
         </div>
     )
 }
--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -43,9 +43,8 @@ export default function Projects () {
     ]
     return (
         <section id="projects" data-section="projects">
-            <div className="h-full bg-slate-950 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] p-4 sm:px-4 md:px-32 lg:px-40">
-                <div className="grid auto-rows-[192px] grid-cols-3 gap-4 p-8">
-                    {/* <Card link={"https://github.com/EliasLeguizamon123"} image={Logo} title="Portfolio" description="Lorem ipsum dolor mutis"  /> */}
+            <div className="h-full bg-slate-950 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] p-4 sm:px-4 md:px-28 lg:px-40">
+                <div className="grid auto-rows-[192px] grid-cols-1 gap-4 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     {projects.map((project, i) => (
                         <Card key={i} i={i} link={project.link} image={project.image} title={project.title} description={project.description} />
                     ))}
