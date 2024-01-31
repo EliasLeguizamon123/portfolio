f95b89262bc7119c253540719584377abc62a327
Elías Leguizamón <elias.leguizamon1997@gmail.com>
2024-01-31 08:40:17 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,15 +13,6 @@ const nextConfig = {
             }
         ]
     },
-    env: {
-        POSTGRES_URL:"postgres://default:9yaedcBiZG6E@ep-steep-art-a4m70ufk-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-        POSTGRES_PRISMA_URL:"postgres://default:9yaedcBiZG6E@ep-steep-art-a4m70ufk-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
-        POSTGRES_URL_NON_POOLING: "postgres://default:9yaedcBiZG6E@ep-steep-art-a4m70ufk.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-        POSTGRES_USER: "default",
-        POSTGRES_HOST: "ep-steep-art-a4m70ufk-pooler.us-east-1.postgres.vercel-storage.com",
-        POSTGRES_PASSWORD: "9yaedcBiZG6E",
-        POSTGRES_DATABASE: "verceldb",
-    }
 };
 
 module.exports = nextConfig;
