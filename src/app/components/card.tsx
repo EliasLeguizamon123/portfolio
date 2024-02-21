34b58092dd7f103e94d20fbeeee51f354f44ff13
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-02-21 15:31:41 -0300

    add github icon in project card

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image"
 import { useEffect, useState } from "react";
 import GenericTag from "./genericTag";
+import { MoveRight } from "lucide-react";
+import Github from "./icons/Github";
 export interface props {
     title: string,
     description: string,
@@ -28,12 +30,12 @@ export default function Card(props: props) {
 
     return (
         <div
-            className={`group row-span-1 cursor-pointer rounded-xl border-2 border-slate-400/10 bg-local text-white transition duration-300 hocus:-translate-y-2.5 hocus:scale-105 ${
+            className={`group row-span-1 cursor-pointer rounded-xl border-2 border-slate-400/10 bg-local text-white transition duration-100 hocus:-translate-y-2.5 hocus:scale-105 ${
                 props.i === 2 || props.i === 3 || props.i === 8 || props.i === 9 ? 'col-span-1 lg:col-span-2' : props.i === 1 || props.i === 5 || props.i === 11 ? 'md:col-span-2 lg:col-span-1' : ''
             }`}
             onClick={() => window.open(props.link, '_blank')}
         >
-            <div className="absolute z-50 hidden w-full flex-col group-hover:flex group-hover:opacity-100">
+            <div className="absolute z-50 hidden w-full flex-col transition duration-200 group-hover:flex group-hover:opacity-100 ">
                 <h2 className="px-4 py-2 text-lg text-white">{props.title}</h2>
                 <div className="flex w-full flex-wrap gap-2 pl-4 text-left">
                     {tags.map((tag, index) => (
@@ -41,6 +43,7 @@ export default function Card(props: props) {
                     ))}
                 </div>
             </div>
+            <p className="absolute bottom-0 z-50 p-4 text-cyan-600"><Github height={'20px'} width={'20px'} color="white" /></p>
             <Image src={props.image} priority={true} style={{objectPosition: "top"}} width={400} alt={props.title} height={700} className="size-full rounded-xl object-cover transition-opacity duration-300 group-hover:brightness-50" />
         </div>
     )
