23b1230ea1ff71ba00fd350f7db210567bc4eb66
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-05-17 17:21:54 -0300

    feat(chat): ABEMUS CHAT

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { Forward } from "lucide-react";
+import React, { useRef, useState } from "react";
+import { sendQuestion } from "../lib/services";
 // import { useState } from "react";
 
 export interface Message {
@@ -9,8 +11,7 @@ export interface Message {
 }
 
 export function Chat () {
-    // const [messages, setMessages] = useState<Message[]>(
-    const messages: Message[] =[
+    const [messages, setMessages] = useState<Message[]>([
         {
             id: Math.random().toString(36).substring(7),
             message: "Hello there!",
@@ -21,12 +22,34 @@ export function Chat () {
             message: "Ask me anything about Elias",
             owner: "bot"
         },
-        {
+    ]);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = new FormData(event.currentTarget);
+
+        setMessages([...messages, {
             id: Math.random().toString(36).substring(7),
-            message: "I am a bot",
+            message: form.get('question') as string,
             owner: "user"
-        },
-    ];
+        }]);
+
+        sendQuestion(form.get('question') as string).then((response) => {
+            console.log(response);
+            setMessages([...messages, {
+                id: Math.random().toString(36).substring(7),
+                message: response,
+                owner: "bot"
+            }]);
+            
+        }).finally(() => {  
+            if (inputRef.current) {
+                inputRef.current.value = '';
+            }
+        })
+    }
     return (
         <>
             <article className="grid max-h-[300px] w-full gap-2 overflow-y-auto rounded-lg border border-gray-800 bg-gradient-to-r from-black to-gray-950 p-4">
@@ -37,18 +60,23 @@ export function Chat () {
                 ))}
             
             </article>
-            <div className='relative mt-4 flex'>
-                <div className='absolute top-0 flex w-full justify-center'>
-                    <div className='h-[1px] w-full animate-border-width rounded-full  bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
+            <form onSubmit={handleSubmit} className="w-full">
+                <div className='relative mt-4 flex'>
+                    <div className='absolute top-0 flex w-full justify-center'>
+                        <div className='h-[1px] w-full animate-border-width rounded-full  bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
+                    </div>
+                    <input
+                        ref={inputRef}
+                        name="question"
+                        autoFocus
+                        className='block h-12 w-[95%] rounded-md rounded-r-none  border border-gray-800 bg-gray-950 px-4 py-2 text-gray-300 focus:outline-none md:px-12'
+                        placeholder='Ask me ...'
+                    />
+                    <button type="submit" className="flex h-12 w-[5%] items-center justify-center rounded-r-md border border-gray-800 bg-gradient-to-r from-black to-gray-950">
+                        <Forward className="text-white" />
+                    </button>
                 </div>
-                <input
-                    className='block h-12 w-[95%] rounded-md  rounded-r-none border border-gray-800 bg-gray-950 px-3 py-2 text-gray-300 focus:outline-none'
-                    placeholder='Ask me ...'
-                />
-                <button className="flex h-12 w-[5%] items-center justify-center rounded-r-md border border-gray-800 bg-gradient-to-r from-black to-gray-950">
-                    <Forward className="text-white" />
-                </button>
-            </div>
+            </form>
         </>
     )
 }
