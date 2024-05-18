984da37c963a608f411f2595f0535a584f29a882
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-05-18 12:08:29 -0300

    fix(chat): add user messages in list

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -26,30 +26,34 @@ export function Chat () {
 
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        
         const form = new FormData(event.currentTarget);
-
-        setMessages([...messages, {
+        const userMessage: Message = {
             id: Math.random().toString(36).substring(7),
             message: form.get('question') as string,
             owner: "user"
-        }]);
+        };
 
-        sendQuestion(form.get('question') as string).then((response) => {
-            console.log(response);
-            setMessages([...messages, {
+        if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+    
+        setMessages(prevMessages => [...prevMessages, userMessage]);
+    
+        sendQuestion(userMessage.message).then((response) => {
+
+            const botMessage: Message = {
                 id: Math.random().toString(36).substring(7),
                 message: response,
                 owner: "bot"
-            }]);
+            };
             
-        }).finally(() => {  
-            if (inputRef.current) {
-                inputRef.current.value = '';
-            }
-        })
-    }
+            setMessages(prevMessages => [...prevMessages, botMessage]);
+        });
+    };
+
     return (
         <>
             <article className="grid max-h-[300px] w-full gap-2 overflow-y-auto rounded-lg border border-gray-800 bg-gradient-to-r from-black to-gray-950 p-4">
@@ -68,9 +72,8 @@ export function Chat () {
                     <input
                         ref={inputRef}
                         name="question"
-                        autoFocus
-                        className='block h-12 w-[95%] rounded-md rounded-r-none  border border-gray-800 bg-gray-950 px-4 py-2 text-gray-300 focus:outline-none md:px-12'
-                        placeholder='Ask me ...'
+                        id="question"
+                        className='block h-12 w-[95%] rounded-md rounded-r-none  border border-gray-800 bg-gray-950 px-4 py-2 text-gray-300 focus:outline-none md:px-6'
                     />
                     <button type="submit" className="flex h-12 w-[5%] items-center justify-center rounded-r-md border border-gray-800 bg-gradient-to-r from-black to-gray-950">
                         <Forward className="text-white" />
