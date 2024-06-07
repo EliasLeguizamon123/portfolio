e18eae066e40c4c148815ab4db72b79b203924fe
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-06-07 13:47:52 -0300

    finish footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
     const date = new Date();
     const actualYear = date.getFullYear();
     return (
-        <footer className="text-white w-full text-center backdrop-blur-sm">
-            <p>{actualYear} &copy; Crafted with ❤️ using <a href="https://nextjs.org" target="_blank" className="text-cyan-700 hocus:text-gray-300">Next</a> by Elias</p>
+        <footer className="w-full text-center text-white backdrop-blur-sm">
+            <p>{actualYear} &copy; Crafted with 💙 using <a href="https://nextjs.org" target="_blank" className="text-cyan-700 hocus:text-gray-300">Next</a> by Elías</p>
         </footer>
     );
 }
