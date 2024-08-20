520cb2bb62a44db5fcf21236181ca523dd270261
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-08-20 16:52:56 -0300

    new icon, change layour and page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,6 @@ const geist = Geist;
 export const metadata: Metadata = {
     title: 'Elias Leguizamon',
     description: 'Created with love by Elias Leguizamon',
-    icons: [
-        { rel: 'icon', url: 'icon.ico' },
-        { rel: 'favicon', url: 'icon.ico' }
-    ]
 }
 
 export default function RootLayout({
@@ -23,6 +19,11 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 {/* <link rel="icon" type="image/svg+xml" href="/icon.png" /> */}
+                <link
+                    rel="icon"
+                    href="/icon.svg"
+                    type="image/svg+xml"
+                />
             </head>
             <body className={geist.className}>
                 <div className='bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)]'>
