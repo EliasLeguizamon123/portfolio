2c51091d03560617b42651bf1589adb5139cb875
Elias <elias.leguizamon1997@gmail.com>
2025-09-01 15:50:40 -0300

    ♻️ refactor: adjust project card sizes for better layout consistency

--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -11,7 +11,7 @@ const mockProjects = [
         link: "https://github.com/EliasLeguizamon123/polychrome",
         tags: ["Rust", "CLI", "Terminal", "Text", "Color"],
         image: "https://github.com/EliasLeguizamon123/polychrome/blob/main/miniature.png?raw=true",
-        size: { colSpan: 2, rowSpan: 2 },
+        size: { colSpan: 1, rowSpan: 2 },
     },
     {
         id: "2",
@@ -20,7 +20,7 @@ const mockProjects = [
         link: "https://github.com/EliasLeguizamon123/Zenith-ide",
         tags: ["Astro", "Electron", "Typescript", "React"],
         image: "https://raw.githubusercontent.com/EliasLeguizamon123/Zenith-ide/refs/heads/main/zenith.webp",
-        size: { colSpan: 2, rowSpan: 1 },
+        size: { colSpan: 1, rowSpan: 1 },
     },
     {
         id: "3",
@@ -29,7 +29,7 @@ const mockProjects = [
         link: "https://www.clocky.online/en",
         tags: ["NextJS", "TailwindCSS", "Typescript", "React"],
         image: "https://github.com/EliasLeguizamon123/RustifyEvents/blob/main/clocky.png?raw=true",
-        size: { colSpan: 1, rowSpan: 1 },
+        size: { colSpan: 2, rowSpan: 2 },
     },
     {
         id: "4",
@@ -38,7 +38,7 @@ const mockProjects = [
         link: "https://github.com/EliasLeguizamon123/wortise-cms",
         tags: ["Next", "Typescript", "Better-Auth", "MongoDB Atlas", "TRPC", "TailwindCSS"],
         image: "https://raw.githubusercontent.com/EliasLeguizamon123/wortise-cms/refs/heads/main/miniature.webp",
-        size: { colSpan: 1, rowSpan: 2 },
+        size: { colSpan: 1, rowSpan: 1 },
     },
     {
         id: "6",
